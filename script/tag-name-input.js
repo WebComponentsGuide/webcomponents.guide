295245ade590d43f216b02f6b128ad2b7ee0dce8
Keith Cirkel <keithamus@users.noreply.github.com>
2022-12-25 16:17:15 +0000

    empty validation when input is cleared

--- script/tag-name-input.js
+++ script/tag-name-input.js
@@ -12,24 +12,24 @@ customElements.define('tag-name-input', class extends HTMLInputElement {
   handleEvent() {
     const {value} = this
     this.setCustomValidity('')
-    if (!value) {
-      return
-    } else if (!(/[A-Z]/.test(value))) {
-      this.setCustomValidity(`${value} is not valid, it cannot contain capital letters`)
-    } else if (reservedTags.has(value)) {
-      this.setCustomValidity(`${value} is not valid, it's a reserved tag`)
-    } if (!(value.includes('-'))) {
-      this.setCustomValidity(`${value} is not valid, it must include a dash (-)`)
-    } else if (value.startsWith('-')) {
-      this.setCustomValidity(`${value} is not valid, it must not start with a dash (-)`)
-    } else if (!(/^[a-z]/.test(value))) {
-      this.setCustomValidity(`${value} is not valid, it must start with a letter (a-z)`)
-    } else if (!validTagName(value)) {
-      const chars = []
-      for (const char of value) {
-        if (!(validChar.test(char))) chars.push(char)
+    if (value) {
+      if (!(/[A-Z]/.test(value))) {
+        this.setCustomValidity(`${value} is not valid, it cannot contain capital letters`)
+      } else if (reservedTags.has(value)) {
+        this.setCustomValidity(`${value} is not valid, it's a reserved tag`)
+      } if (!(value.includes('-'))) {
+        this.setCustomValidity(`${value} is not valid, it must include a dash (-)`)
+      } else if (value.startsWith('-')) {
+        this.setCustomValidity(`${value} is not valid, it must not start with a dash (-)`)
+      } else if (!(/^[a-z]/.test(value))) {
+        this.setCustomValidity(`${value} is not valid, it must start with a letter (a-z)`)
+      } else if (!validTagName(value)) {
+        const chars = []
+        for (const char of value) {
+          if (!(validChar.test(char))) chars.push(char)
+        }
+        this.setCustomValidity(`${value} is not a valid tag name, cannot contain ${chars.join(', ')}!`)
       }
-      this.setCustomValidity(`${value} is not a valid tag name, cannot contain ${chars.join(', ')}!`)
     }
     this.reportValidity()
     this.parentElement.querySelector('.error').textContent = this.validationMessage || 'Valid custom element name!'
