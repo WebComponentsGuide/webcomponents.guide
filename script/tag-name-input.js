eb7ad9c289c2c403146ac43e477274b29d81f2f4
Keith Cirkel <keithamus@users.noreply.github.com>
2022-12-25 16:26:20 +0000

    empty error message out on empty value in tag-name-input

--- script/tag-name-input.js
+++ script/tag-name-input.js
@@ -32,7 +32,7 @@ customElements.define('tag-name-input', class extends HTMLInputElement {
       }
     }
     this.reportValidity()
-    this.parentElement.querySelector('.error').textContent = this.validationMessage || 'Valid custom element name!'
+    this.parentElement.querySelector('.error').textContent = this.validationMessage || value ? 'Valid custom element name!' : ''
   }
 
 }, { extends: 'input' })
