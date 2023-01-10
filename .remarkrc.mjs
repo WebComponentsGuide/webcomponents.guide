085b5e2b6f047d1ed71668a5dfcb54760cf71f10
Keith Cirkel <keithamus@users.noreply.github.com>
2023-01-10 17:56:44 +0000

    add retext-readability

--- .remarkrc.mjs
+++ .remarkrc.mjs
@@ -1,5 +1,5 @@
-import retext from 'remark-retext'
-import {Parser} from 'retext-english'
+import retext from "remark-retext"
+import { Parser } from "retext-english"
 export default {
   plugins: [
     "remark-lint",
@@ -18,6 +18,7 @@ export default {
     ["remark-lint-no-shell-dollars", false],
     ["remark-lint-maximum-heading-length", 73],
     [retext, Parser],
-    "retext-indefinite-article"
+    "retext-indefinite-article",
+    ["retext-readability", { threshold: 6 / 7 }],
   ],
 }
